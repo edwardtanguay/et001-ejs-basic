3ab65fcb1f2f7a696d60266e7a2bb971708ab5ca
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-13 00:09:55 +0100

    step 4: static site

--- dist/server.js
+++ dist/server.js
@@ -1,20 +1,9 @@
 import express from 'express';
+import path from 'path';
 const app = express();
+const __dirname = path.resolve(path.dirname(''));
 const port = 3890;
-app.get('/', (req, res) => {
-    req.
-        res.send(`
-	 	<h1>Home</h1>
-		<p>go to the <a href="/info">info page</a></p>
-	`);
-});
-app.get('/info', (req, res) => {
-    req.
-        res.send(`
-	 	<h1>Info</h1>
-		<p>go back <a href="/">home</a></p>
-	`);
-});
+app.use(express.static(__dirname + '/public'));
 app.listen(port, () => {
     console.log(`listening on http://localhost:${port}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,11 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
+const __dirname = path.resolve(path.dirname(''));
 const port = 3890;
 
-app.get('/', (req: express.Request, res: express.Response) => {
-	req.
-	res.send(`
-	 	<h1>Home</h1>
-		<p>go to the <a href="/info">info page</a></p>
-	`);
-});
-
-app.get('/info', (req: express.Request, res: express.Response) => {
-	req.
-	res.send(`
-	 	<h1>Info</h1>
-		<p>go back <a href="/">home</a></p>
-	`);
-});
+app.use(express.static(__dirname + '/public'));
 
 app.listen(port, () => {
 	console.log(`listening on http://localhost:${port}`);
