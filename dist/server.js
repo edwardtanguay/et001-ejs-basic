957275aa514c7d5e7b09f60fe7a1e6e4e8dfabd3
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-12 23:46:18 +0100

    step 3

--- dist/server.js
+++ dist/server.js
@@ -1,4 +1,21 @@
-const message = 'TypeScript works, great.';
-console.log(message);
-export {};
+import express from 'express';
+const app = express();
+const port = 3890;
+app.get('/', (req, res) => {
+    req.
+        res.send(`
+	 	<h1>Home</h1>
+		<p>go to the <a href="/info">info page</a></p>
+	`);
+});
+app.get('/info', (req, res) => {
+    req.
+        res.send(`
+	 	<h1>Info</h1>
+		<p>go back <a href="/">home</a></p>
+	`);
+});
+app.listen(port, () => {
+    console.log(`listening on http://localhost:${port}`);
+});
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,24 @@
-const message: string = 'TypeScript works, great.';
-console.log(message);
+import express from 'express';
+
+const app = express();
+const port = 3890;
+
+app.get('/', (req: express.Request, res: express.Response) => {
+	req.
+	res.send(`
+	 	<h1>Home</h1>
+		<p>go to the <a href="/info">info page</a></p>
+	`);
+});
+
+app.get('/info', (req: express.Request, res: express.Response) => {
+	req.
+	res.send(`
+	 	<h1>Info</h1>
+		<p>go back <a href="/">home</a></p>
+	`);
+});
+
+app.listen(port, () => {
+	console.log(`listening on http://localhost:${port}`);
+});
