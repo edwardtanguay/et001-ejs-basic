7f52a0daf3193670e17858c81845b6596ca1f08b
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-13 00:30:53 +0100

    step5 ejs basic

--- dist/server.js
+++ dist/server.js
@@ -4,6 +4,14 @@ const app = express();
 const __dirname = path.resolve(path.dirname(''));
 const port = 3890;
 app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './public/views'));
+app.get('/', (req, res) => {
+    res.render('index');
+});
+app.get('/info', (req, res) => {
+    res.render('info');
+});
 app.listen(port, () => {
     console.log(`listening on http://localhost:${port}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,16 @@ const __dirname = path.resolve(path.dirname(''));
 const port = 3890;
 
 app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './public/views'));
+
+app.get('/', (req: express.Request, res: express.Response) => {
+	res.render('index');
+});
+
+app.get('/info', (req: express.Request, res: express.Response) => {
+	res.render('info');
+});
 
 app.listen(port, () => {
 	console.log(`listening on http://localhost:${port}`);
