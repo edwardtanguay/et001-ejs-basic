4eef20bd420b63b6f772d8560d0a0e7401c76ae3
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-13 01:37:36 +0100

    step 7 menu

--- dist/server.js
+++ dist/server.js
@@ -6,12 +6,24 @@ const port = 3890;
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './public/views'));
-const appTitle = 'Tech Book Club';
+const siteData = {
+    appTitle: 'Tech Book Club',
+    pages: [
+        {
+            title: 'Home',
+            path: '/'
+        },
+        {
+            title: 'Info',
+            path: '/info'
+        }
+    ]
+};
 app.get('/', (req, res) => {
-    res.render('index', { appTitle });
+    res.render('index', { siteData, currentPath: '/' });
 });
 app.get('/info', (req, res) => {
-    res.render('info', { appTitle });
+    res.render('info', { siteData, currentPath: '/info' });
 });
 app.listen(port, () => {
     console.log(`listening on http://localhost:${port}`);
